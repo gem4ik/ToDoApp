ee194e4ec4bbad10864fff8acb813a86774c4a94
gem4ik <mgemskij@gmail.com>
2023-07-19 00:21:41 +0300

    refactor genericResponceType, use axios.create

--- src/components/reduce/TodolistAPI.ts
+++ src/components/reduce/TodolistAPI.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-export const settings = {
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     header: {
         'API-KEY': '31d3b244-d207-4062-b145-297089d8f2c1'
     }
-}
+})
 
 export type TodolistType = {
     id: string
@@ -13,41 +14,27 @@ export type TodolistType = {
     addedDate: string
     order: number
 }
-export type CreateTodolistResponceType ={
+export type ResponceType<D> = {
     resultCode: number
     message: string[]
-    data: {
-        item: TodolistType
-    }
+    data: D
 }
-export type DeleteTodolistResponceType ={
-    resultCode: number
-    message: string[]
-    data: {}
-}
-export type UpdateTodolistResponceType ={
-    resultCode: number
-    message: string[]
-    data: {}
-}
+
 export const TodolistAPI = {
     getData() {
-        return axios.get<TodolistType[]>('https://social-network.samuraijs.com/api/1.1//todo-lists', settings)
+        return instance.get<TodolistType[]>('todo-lists')
     },
     post() {
-        return axios.post<CreateTodolistResponceType>('https://social-network.samuraijs.com/api/1.1//todo-lists',
-            {title: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"},
-            settings)
+        return instance.post<ResponceType<{item: TodolistType}>>('todo-lists',
+            {title: "DDDDDDDDDDD"})
     },
     put() {
-        const todolistId = 'b3d0b4d2-4d0c-4f04-9ba5-a7c60f384b4d'
-        return axios.put<UpdateTodolistResponceType>(`https://social-network.samuraijs.com/api/1.1//todo-lists/${todolistId}`,
-            {title: "CCCCCCCCCCCCCCC"},
-            settings)
+        const todolistId = 'c5e5250f-83d4-4eb0-88f5-b2debd14c5d5'
+        return instance.put<ResponceType<{}>>(`todo-lists/${todolistId}`,
+            {title: "EEEEEEEEE"},)
     },
     delete() {
-        const todolistId = 'b3d0b4d2-4d0c-4f04-9ba5-a7c60f384b4d'
-        return axios.delete<DeleteTodolistResponceType>(`https://social-network.samuraijs.com/api/1.1//todo-lists/${todolistId}`,
-            settings)
+        const todolistId = 'c5e5250f-83d4-4eb0-88f5-b2debd14c5d5'
+        return instance.delete<ResponceType<{}>>(`todo-lists/${todolistId}`,)
     }
 }
